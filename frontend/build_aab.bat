3bd2e2506cdaf8b914ac949f08693aacf23aa1ee
lokesh <lokeshlovewanshi98@gmail.com>
2026-09-20 15:10:07 +0530

    fix issues

--- frontend/build_aab.bat
+++ frontend/build_aab.bat
@@ -3,3 +3,4 @@ set EXPO_TOKEN=4tuLb4WhRpro7J3PMs2JSFpmdzaaaSlKp_v2iM76
 cd /d "%~dp0"
 echo Triggering EAS production AAB build for Google Play Console...
 node "C:\Users\lokes\AppData\Roaming\npm\node_modules\eas-cli\bin\run" build --platform android --profile production --non-interactive --no-wait
+
